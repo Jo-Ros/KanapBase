dfcd62f359ce906c385bd46af9c3b5d0b9df3f6e
Jo-Ros <94191307+Jo-Ros@users.noreply.github.com>
2022-04-02 19:53:58 +0200

    refactor: refactoring code / add functions

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,111 @@
 console.log('hello');
 const itemContainer = document.querySelector('.item');
 const pageTitle = document.querySelector('title');
-let currentProductId = location.search.substring(4);
+const currentProductId = location.search.substring(4);
+const productUrl = `http://localhost:3000/api/products/${currentProductId}`;
 console.log(currentProductId);
 
-fetch(`http://localhost:3000/api/products/${currentProductId}`)
+// fetch(`http://localhost:3000/api/products/${currentProductId}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         // productData = data;
+//         console.log(data)
+//         pageTitle.innerText = `${data.name}`
+//         const productInfos = document.createElement('article');
+//         productInfos.innerHTML = `
+//             <div class="item__img">
+//             <img src="${data.imageUrl}" alt="${data.altTxt}">
+//             </div>
+//             <div class="item__content">
+
+//             <div class="item__content__titlePrice">
+//                 <h1 id="title">${data.name}</h1>
+//                 <p>Prix : <span id="price">${data.price}</span>€</p>
+//             </div>
+
+//             <div class="item__content__description">
+//                 <p class="item__content__description__title">Description :</p>
+//                 <p id="description">${data.description}</p>
+//             </div>
+
+//             <div class="item__content__settings">
+//                 <div class="item__content__settings__color">
+//                 <label for="color-select">Choisir une couleur :</label>
+//                 <select name="color-select" id="colors">
+//                     ${data.colors.map(
+//                         (color) => "<option value=" + color + ">" + color + "</option>"
+//                     )}
+//                 </select>
+//                 </div>
+
+//                 <div class="item__content__settings__quantity">
+//                 <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+//                 <input type="number" name="itemQuantity" min="1" max="100" value="1" id="quantity">
+//                 </div>
+//             </div>
+
+//             <div class="item__content__addButton">
+//                 <button id="addToCart">Ajouter au panier</button>
+//             </div>
+
+//             </div>`;
+            
+//             itemContainer.appendChild(productInfos);
+
+//             //=========================================================
+
+//             const addToCartBtn = document.querySelector('#addToCart');
+//             const colors = document.querySelector('#colors');
+//             const quantity = document.querySelector('#quantity');
+
+//             addToCartBtn.addEventListener('click', (e) => {
+//                 e.preventDefault();
+//                 let chosenColor = colors.value;
+//                 console.log(chosenColor);
+//                 let quantityAdded = quantity.value;
+//                 console.log(quantityAdded);
+
+//                 let chosenProduct = {
+//                     name: `${data.name}`,
+//                     id: `${currentProductId}`,
+//                     color: chosenColor,
+//                     quantity: quantityAdded
+//                 }
+
+//                 //=========================================================
+
+//                 let productsInCart =  JSON.parse(localStorage.getItem("products"));
+//                 console.log(productsInCart);
+
+//                 if(productsInCart) {
+//                     productsInCart.push(chosenProduct);
+//                     localStorage.setItem("products", JSON.stringify(productsInCart));
+//                 } else {
+//                     productsInCart = [];
+//                     productsInCart.push(chosenProduct);
+//                     localStorage.setItem("products", JSON.stringify(productsInCart));
+
+//                     console.log(productsInCart);
+//                 }
+//             })
+
+
+//     }) .catch((err) => alert(err))
+
+
+apiCall(productUrl);
+function apiCall(url) {
+    fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        // productData = data;
-        console.log(data)
+        console.log(data);
+        showProduct(data)
+        chooseItem(data)
+    })
+    .catch((err) => alert(err))
+}
+
+function showProduct(data) {
         pageTitle.innerText = `${data.name}`
         const productInfos = document.createElement('article');
         productInfos.innerHTML = `
@@ -50,42 +147,45 @@ fetch(`http://localhost:3000/api/products/${currentProductId}`)
             </div>`;
             
             itemContainer.appendChild(productInfos);
+}
 
-            const addToCartBtn = document.querySelector('#addToCart');
-            const colors = document.querySelector('#colors');
-            const quantity = document.querySelector('#quantity');
+function chooseItem(data) {
+    const addToCartBtn = document.querySelector('#addToCart');
+    const colors = document.querySelector('#colors');
+    const quantity = document.querySelector('#quantity');
 
-            addToCartBtn.addEventListener('click', (e) => {
-                e.preventDefault();
-                let chosenColor = colors.value;
-                console.log(chosenColor);
-                let quantityAdded = quantity.value;
-                console.log(quantityAdded);
+    addToCartBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let chosenColor = colors.value;
+        console.log(chosenColor);
+        let quantityAdded = quantity.value;
+        console.log(quantityAdded);
 
-                let chosenProduct = {
-                    name: `${data.name}`,
-                    id: `${currentProductId}`,
-                    color: chosenColor,
-                    quantity: quantityAdded
-                }
+        let chosenProduct = {
+            name: `${data.name}`,
+            id: `${currentProductId}`,
+            color: chosenColor,
+            quantity: quantityAdded
+        }
+        setInCart(chosenProduct);
+    })
+}
 
-                let productsInCart =  JSON.parse(localStorage.getItem("products"));
-                console.log(productsInCart);
+function setInCart(chosenProduct) {
+    let productsInCart =  JSON.parse(localStorage.getItem("products"));
+    console.log(productsInCart);
 
-                if(productsInCart) {
-                    productsInCart.push(chosenProduct);
-                    localStorage.setItem("products", JSON.stringify(productsInCart));
-                } else {
-                    productsInCart = [];
-                    productsInCart.push(chosenProduct);
-                    localStorage.setItem("products", JSON.stringify(productsInCart));
+    if(productsInCart) {
+        productsInCart.push(chosenProduct);
+        localStorage.setItem("products", JSON.stringify(productsInCart));
+    } else {
+        productsInCart = [];
+        productsInCart.push(chosenProduct);
+        localStorage.setItem("products", JSON.stringify(productsInCart));
 
-                    console.log(productsInCart);
-                }
-            })
-
-
-    }) .catch((err) => alert(err))
+        console.log(productsInCart);
+    }
+}
 
 // const dataProduct = {color, quantity};
 // const postOptions = {
--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,36 @@
 console.log('hello');
 const itemsContainer = document.querySelector(".items");
+const API_URL = `http://localhost:3000/api/products`;
 
 // ===================================
-fetch("http://localhost:3000/api/products")
-    .then((res) => res.json()
+apiCall(API_URL);
+
+function apiCall(url) {
+    fetch(url)
+    .then((res) => res.json())
     .then((data) => {
-
-        data.forEach(product => {
-            
-            const {altTxt, description, imageUrl, name, _id} = product;
-            let productUrl = `./product.html?id=${_id}`;
-            //console.log(productUrl);
-            const productElement = document.createElement('div');
-            productElement.classList.add("item");
-            productElement.innerHTML = `
-                <a href="${productUrl}">
-                    <article>
-                        <img src="${imageUrl}" alt="${altTxt}">
-                        <h3 class="productName">${name}</h3>
-                        <p class="productDescription">${description}</p>
-                    </article>
-                </a>`;
-    
-            itemsContainer.appendChild(productElement);
-        })
+        showProducts(data)
     })
-    )
-    .catch((err) => alert(err));
+    .catch((err) => alert(err))
+}
 
-// ===================================
-
-// _id, altTxt, description, imageUrl, name, colors, price
-
-
-
+function showProducts(data) {
+    data.forEach(product => {
+            
+        const {altTxt, description, imageUrl, name, _id} = product;
+        let productUrl = `./product.html?id=${_id}`;
+        console.log(productUrl);
+        const productElement = document.createElement('div');
+        productElement.classList.add("item");
+        productElement.innerHTML = `
+        <a href="${productUrl}">
+            <article>
+                <img src="${imageUrl}" alt="${altTxt}">
+                <h3 class="productName">${name}</h3>
+                <p class="productDescription">${description}</p>
+            </article>
+        </a>`;
+    
+        itemsContainer.appendChild(productElement);
+    })
+}
